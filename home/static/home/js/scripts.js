e5479f94001306dff9d5156568027e1202697272
screw-pack <gaillard.germain@gmail.com>
2021-06-05 07:51:16 +0200

    Created home app. Created templates for base and index pages. Added statics.

---
+++ home/static/home/js/scripts.js
@@ -0,0 +1,22 @@
+function responsiveMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("home-header");
+
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
