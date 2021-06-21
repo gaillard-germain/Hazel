4ab2709ff5a53f0d523fff38db4c8f99ee0835ad
screw-pack <gaillard.germain@gmail.com>
2021-06-21 16:40:08 +0200

    Added make_agenda test.

--- home/tests.py
+++ home/tests.py
@@ -1,9 +1,29 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.template.defaultfilters import date as _date
+from datetime import date, datetime
+from .models import Agenda, Category, Activity
 
 
 class IndexPageTestCase(TestCase):
 
+    def setUp(self):
+        self.agenda = Agenda.objects.create(entry=date.today())
+        self.category = Category.objects.create(name='FAKE')
+        self.activity = Activity.objects.create(
+            name='Fake activity',
+            day=self.agenda,
+            start_time=datetime.now().time(),
+            end_time=datetime.now().time()
+        )
+        self.activity.categories.add(self.category)
+
     def test_index_page_returns_200(self):
         response = self.client.get(reverse('home:index'))
         self.assertEqual(response.status_code, 200)
+
+    def test_make_agenda_method(self):
+        agenda = Agenda.make_agenda()
+        day = _date(self.agenda.entry, 'D d F')
+        result = self.activity.name
+        self.assertEqual(agenda[self.category][day][0].name, result)
