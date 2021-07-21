8fbe0b087ec26105e536fa4e6e3cad46489ba054
screw-pack <gaillard.germain@gmail.com>
2021-07-21 12:36:53 +0200

    added custom filter to BookingAdmin.

--- booking/admin.py
+++ booking/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Period, Booking, Slot
 from registration.models import Child
 from home.models import Category
-from registration.admin import ChildInline
 
 
 class BookingInLine(admin.TabularInline):
@@ -23,6 +22,34 @@ class PeriodAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date')
 
 
+class GroupListFilter(admin.SimpleListFilter):
+    title = ('Groupe')
+    parameter_name = 'groupe'
+
+    def lookups(self, request, model_admin):
+        categories = Category.objects.all()
+        tuples_list = []
+        for category in categories:
+            tuples_list.append((category.name, (category.name)))
+
+        return tuples_list
+
+    def queryset(self, request, queryset):
+        if self.value():
+            category = Category.objects.get(name=self.value())
+            children = Child.objects.exclude(category=category)
+            for child in children:
+                queryset = queryset.exclude(child=child)
+
+            return queryset
+
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('child', 'slot', 'whole', 'validated')
+    list_display = ('child', 'groupe', 'slot', 'whole', 'validated')
+    list_editable = ('whole', 'validated')
+    list_filter = ('slot',GroupListFilter)
+    ordering = ('slot',)
+
+    def groupe(self, obj):
+        child = Child.objects.get(booking=obj)
+        return child.category
--- registration/admin.py
+++ registration/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from .models import Family, Child, Adult
 
 
-
-class ChildInline(admin.TabularInline):
-    model = Child
-    extra = 0
-    fields = ('firstname', 'lastname', 'category')
-
 @admin.register(Child)
 class ChildAdmin(admin.ModelAdmin):
     search_fields = ('lastname', 'firstname',)
