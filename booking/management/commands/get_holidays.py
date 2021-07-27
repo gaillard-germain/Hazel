a8c55b70eceff6925c14352cf04c90207732e830
screw-pack <gaillard.germain@gmail.com>
2021-07-27 15:19:54 +0200

    fixed get_holidays script.

--- booking/management/commands/get_holidays.py
+++ booking/management/commands/get_holidays.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 import requests
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -12,7 +13,7 @@ class Command(BaseCommand):
         ./manage.py get_holidays to run (! it will delete former periods!) """
 
     def get_school_year(self):
-        if date.today().month < 9:
+        if date.today().month < 8:
             year1 = (date.today() - relativedelta(years=1)).year
             year2 = date.today().year
         else:
@@ -27,11 +28,9 @@ class Command(BaseCommand):
         query = 'https://data.education.gouv.fr/api/records/1.0/search/?\
 dataset=fr-en-calendrier-scolaire&q=&facet=description&\
 facet=population&facet=start_date&facet=end_date&\
-facet=location&facet=zones&\
-facet=annee_scolaire&refine.population=%C3%89l%C3%A8ves&\
+facet=location&facet=zones&facet=annee_scolaire&\
 refine.location=Bordeaux&refine.zones=Zone+A&\
-refine.annee_scolaire={}-{}&\
-timezone=Europe%2FParis'.format(year1, year2)
+refine.annee_scolaire={}-{}&timezone=Europe%2FParis'.format(year1, year2)
 
         response = requests.get(query)
         response = response.json()
@@ -40,17 +39,39 @@ timezone=Europe%2FParis'.format(year1, year2)
         self.stdout.write(self.style.SUCCESS('Deleted former periods'))
 
         for record in response['records']:
-            fields = record['fields']
-            name = fields['description']
-            start_date = date.fromisoformat(fields['start_date'].split('T')[0])
-            end_date = date.fromisoformat(fields['end_date'].split('T')[0])
+            try:
+                fields = record['fields']
 
-            period = Period.objects.create(name=name, start_date=start_date,
-                                           end_date=end_date)
+                if (fields['population'] == '-' or
+                        fields['population'] == 'Élèves'):
 
-            self.stdout.write(self.style.SUCCESS(
-                'Successfully create {} period'.format(period.name))
-            )
+                    name = fields['description']
+                    start_date = date.fromisoformat(
+                        fields['start_date'].split('T')[0]
+                    )
+                    end_date = date.fromisoformat(
+                        fields['end_date'].split('T')[0]
+                    )
+
+                    period = Period.objects.create(
+                        name=name,
+                        start_date=start_date,
+                        end_date=end_date
+                    )
+
+                    self.stdout.write(self.style.SUCCESS(
+                        "Successfully create '{}' period".format(period.name))
+                    )
+
+            except KeyError as error:
+                self.stdout.write(self.style.ERROR(
+                    'Failed to create because of KeyError {}'.format(error)
+                ))
+
+            except IntegrityError as error:
+                self.stdout.write(self.style.ERROR(
+                    '{}'.format(error)
+                ))
 
         start_date = date.fromisoformat('{}-09-01'.format(year1))
         end_date = date.fromisoformat('{}-07-01'.format(year2))
@@ -60,5 +81,5 @@ timezone=Europe%2FParis'.format(year1, year2)
                                        end_date=end_date)
 
         self.stdout.write(self.style.SUCCESS(
-                'Successfully create {} period'.format(period.name))
+                "Successfully create '{}' period".format(period.name))
             )
