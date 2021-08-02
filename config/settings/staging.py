c31a65422aad29cdab158d0975cf7a4084986008
screw-pack <gaillard.germain@gmail.com>
2021-08-02 11:23:52 +0200

    added travis file

---
+++ config/settings/staging.py
@@ -0,0 +1,12 @@
+SECRET_KEY = '%q+!zollpuq)u8izg42lc=k)!!9n-0zl5p5&$p07*(nlr0vo%$'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
