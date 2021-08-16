6ddc5b36e92920ca525aa9b015b9d000fa64ec7e
screw-pack <gaillard.germain@gmail.com>
2021-08-16 11:46:20 +0200

    added more tests

--- home/tests.py
+++ home/tests.py
@@ -4,11 +4,15 @@ from django.template.defaultfilters import date as _date
 from datetime import date, datetime
 from registration.models import Category
 from booking.models import Slot, Activity
+from .models import Price
+import decimal
 
 
 class IndexPageTestCase(TestCase):
 
     def setUp(self):
+        self.price = Price.objects.create(family_quotient='fake',
+                                          day=2.55, half_day=1.65)
         self.slot = Slot.objects.create(day=date.today())
         self.category = Category.objects.create(name='FAKE', age_min=5,
                                                 age_max=8)
@@ -30,6 +34,18 @@ class IndexPageTestCase(TestCase):
         result = self.activity.name
         self.assertEqual(
             response.context['agenda'][self.category][day][0].name, result
+        )
+
+    def test_index_page_prices_conform_to_expected(self):
+        response = self.client.get(reverse('home:index'))
+        print('AAAh')
+        print(type(response.context['prices'][0].day))
+        self.assertEqual(
+            response.context['prices'][0].family_quotient,
+            self.price.family_quotient
+        )
+        self.assertIsInstance(
+            response.context['prices'][0].day, decimal.Decimal
         )
 
 
