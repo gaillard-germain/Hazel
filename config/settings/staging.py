f5e889fb1cbdf08fd8fb59347eab8fe774cfb1a1
screw-pack <gaillard.germain@gmail.com>
2021-08-26 09:26:45 +0200

    Rewrited config/settings to avoid wildcard imports and relative imports

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,5 +1,9 @@
 SECRET_KEY = '%q+!zollpuq)u8izg42lc=k)!!9n-0zl5p5&$p07*(nlr0vo%$'
 
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
